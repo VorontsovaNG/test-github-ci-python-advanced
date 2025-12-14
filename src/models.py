0f54fa045231a7aad87fb8cb422297a31a94104f
Natalia <natalya.gerasimenko@gmail.com>
2025-12-14 20:23:44 +0300

    Updated models.py

--- src/models.py
+++ src/models.py
@@ -1,27 +1,30 @@
-from .app import db
-from typing import Dict, Any
+from typing import Any, Dict
+
 from sqlalchemy.ext.associationproxy import association_proxy
+
+from .app import db
 
 
 def my_strtobool(val: str) -> bool:
-
-    """ Преобразует строку в булевое значение """
+    """Преобразует строку в булевое значение"""
 
     val = val.lower()
 
-    true_values = {'1', 'yes', 'y', 'true', 't', 'on'}
-    false_values = {'0', 'no', 'n', 'false', 'f', 'off'}
+    true_values = {"1", "yes", "y", "true", "t", "on"}
+    false_values = {"0", "no", "n", "false", "f", "off"}
 
     if val in true_values:
         return True
     elif val in false_values:
         return False
     else:
-        raise ValueError(f"Недопустимое значение '{val}' для преобразования в булевое значение.")
+        raise ValueError(
+            f"Недопустимое значение '{val}' для преобразования в булевое значение."
+        )
 
 
 class Client(db.Model):
-    __tablename__ = 'client'
+    __tablename__ = "client"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -29,19 +32,18 @@ class Client(db.Model):
     credit_card = db.Column(db.String(50))
     car_number = db.Column(db.String(10))
 
-    client_parkings = db.relationship('ClientParking', back_populates='driver')
-    parkings = association_proxy('client_parkings', 'parking_place')
+    client_parkings = db.relationship("ClientParking", back_populates="driver")
+    parkings = association_proxy("client_parkings", "parking_place")
 
     def __repr__(self):
         return f"Клиент {self.name + ' ' + self.surname}"
 
     def to_json(self) -> Dict[str, Any]:
-        return {c.name: getattr(self, c.name) for c in
-                self.__table__.columns}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class Parking(db.Model):
-    __tablename__ = 'parking'
+    __tablename__ = "parking"
 
     id = db.Column(db.Integer, primary_key=True)
     address = db.Column(db.String(100), nullable=False)
@@ -49,38 +51,40 @@ class Parking(db.Model):
     count_places = db.Column(db.Integer, nullable=False)
     count_available_places = db.Column(db.Integer, nullable=False)
 
-    clients = db.relationship('ClientParking', back_populates='parking_place')
-    park_clients = association_proxy('clients', 'driver')
-
+    clients = db.relationship("ClientParking", back_populates="parking_place")
+    park_clients = association_proxy("clients", "driver")
 
     def __repr__(self):
-        return ('Парковка № {id}\nАдрес: {address}\nВсего парковочных мест: {total_places}\n'
-                'Свободно парковочных мест: {free_places}\nПарковка: {status}').format(
+        return (
+            "Парковка № {id}\nАдрес: {address}\nВсего парковочных мест: {total_places}\n"
+            "Свободно парковочных мест: {free_places}\nПарковка: {status}"
+        ).format(
             id=self.id,
             address=self.address,
             total_places=self.count_places,
             free_places=self.count_available_places,
-            status="ОТКРЫТА" if self.opened else "ЗАКРЫТА"
-            )
+            status="ОТКРЫТА" if self.opened else "ЗАКРЫТА",
+        )
 
     def to_json(self) -> Dict[str, Any]:
-        return {c.name: getattr(self, c.name) for c in
-                self.__table__.columns}
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 
 class ClientParking(db.Model):
-    __tablename__ = 'client_parking'
+    __tablename__ = "client_parking"
 
     id = db.Column(db.Integer, primary_key=True)
     time_in = db.Column(db.DateTime)
     time_out = db.Column(db.DateTime)
-    client_id = db.Column(db.Integer, db.ForeignKey('client.id'))
-    parking_id = db.Column(db.Integer, db.ForeignKey('parking.id'))
+    client_id = db.Column(db.Integer, db.ForeignKey("client.id"))
+    parking_id = db.Column(db.Integer, db.ForeignKey("parking.id"))
 
-    __table_args__ = (db.UniqueConstraint('client_id', 'parking_id', name='unique_client_parking'),)
+    __table_args__ = (
+        db.UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),
+    )
 
-    driver = db.relationship('Client', back_populates='client_parkings')
-    parking_place = db.relationship('Parking', back_populates='clients')
+    driver = db.relationship("Client", back_populates="client_parkings")
+    parking_place = db.relationship("Parking", back_populates="clients")
 
     def __repr__(self):
         return f"{self.driver} заехал на парковку № {self.parking_id} {self.time_in}."
