94f73bd78a84651ec2b8b65666dc3f1e5e9ac684
VorontsovaNG <natalya.gerasimenko@gmail.com>
2025-12-14 04:14:48 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 import pytest
-from ..src.app import create_app, db as _db
-from ..src.models import Client, ClientParking, Parking
+from src.app.py import create_app, db as _db
+from src.models import Client, ClientParking, Parking
 from datetime import datetime
 
 
